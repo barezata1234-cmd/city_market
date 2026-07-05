40de6b41175e4a81b71202ff7054119067f9c828
Barez <email  >
2026-07-05 11:00:21 +0300

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,10 @@
 <?php
+use Illuminate\Support\Facades\URL;
 
-namespace App\Providers;
+public function boot(): void
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- دڵنیابە ئەم هێڵە لێرەیە
-
-class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        // ناچارکردنی پڕۆژەکە بۆ بەکارهێنانی HTTPS کاتێک لەسەر سێرڤەرە
-        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
 }
